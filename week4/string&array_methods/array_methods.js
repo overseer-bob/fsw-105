eaf08c95c6a6a4a4eb9e95dfd9e340d3c747deb8
overseer-bob <60929931+overseer-bob@users.noreply.github.com>
2020-05-03 21:20:24 -0500

    week4

---
+++ week4/string&array_methods/array_methods.js
@@ -0,0 +1,26 @@
+let fruit = ["banana", "apple", "orange", "watermelon"];
+let vegetabels = ["carrot", "tomato", "pepper", "lettuce"];
+
+function arrayExercise() {
+    vegetabels.pop();
+    //console.log("vegetables: ", vegetabels);
+    fruit.shift();
+    //console.log("fruit: ", fruit);
+    let indexOfOrange = fruit.indexOf("orange");
+    //console.log(indexOfOrange);
+    fruit.push(indexOfOrange);
+    //console.log("fruit: ", fruit);
+    let lengthOfVegetablesArray = vegetabels.length;
+    //console.log(lengthOfVegetablesArray);
+    vegetabels.push(lengthOfVegetablesArray);
+    //console.log("vegetables: ", vegetabels);
+    let food = fruit.concat(vegetabels);
+    //console.log(food);
+    food.splice(4, 2);
+    //console.log(food);
+    food.reverse();
+    //console.log(food);
+    return food.join();
+}
+
+console.log(arrayExercise());
